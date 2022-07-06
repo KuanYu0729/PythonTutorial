a42311836e1aef82bb5c934f952c3e9942d244bc
KuanYu <george310909@gmail.com>
2022-07-06 21:11:02 +0800

    Add if else tutorial

---
+++ 3.if_else.py
@@ -0,0 +1,11 @@
+num1 = 1
+num2 = 2
+# if else 檢查順序會由 if > else if > else if > ... > else
+# if 
+if num1 < num2:
+	print("num2 比較大")
+# else if 
+elif num1 == num2:
+	print("一樣大")
+else:
+	print("num1 比較大")
